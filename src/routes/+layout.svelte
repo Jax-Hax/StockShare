d97e17c6bfa37034446f07bc97b16647e822b73d
Jax-Hax <jaxhacks543@gmail.com>
2023-08-05 16:34:46 -0700

    added layout and set up header

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,45 @@
+<html lang="en">
+	<style>
+		:root {
+			--green: #26b54c;
+		}
+		* {
+			margin: 0;
+			padding: 0;
+			font-family: 'Source Sans 3', sans-serif;
+			color: black;
+		}
+		h1,
+		h2 {
+			font-family: 'Oswald', sans-serif;
+		}
+		button {
+			background-color: var(--green);
+			border: none;
+			color: white;
+			padding: 1em 2em;
+			border-radius: 19px;
+			text-align: center;
+			text-decoration: none;
+			cursor: pointer;
+			display: inline-block;
+			font-size: 1rem;
+		}
+		button:hover {
+			background-color: white;
+			box-shadow: 0 3px;
+			color: var(--green);
+			transform: translateY(-0.25em);
+			transition: transform 0.25s;
+			border: -2px solid var(--green);
+			border-width: 1px;
+			border-style: solid;
+			margin: -1px;
+		}
+		h1 {
+			font-size: 3.5rem;
+			letter-spacing: -3px;
+		}
+	</style>
+<slot />
+</html>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<svelte:head>
+	<title>StockShare - Practice Trading Stocks with Friends</title>
+	<meta
+		name="description"
+		content="Learn about the stock market without risk. Invite your friends to a competition for free!"
+	/>
+</svelte:head>
+<header>
+	<img src="/favicon.png" alt="the logo for StockShare" style="width: min(9vw,5em)" />
+	<h1 id="nav-title"><a href="/" style="text-decoration: none">StockShare</a></h1>
+	<div>
+		<a href="/contact" style="padding-right: 0.5em"><button>Get Started</button></a>
+		<a href="/contact"><button>Log In</button></a>
+	</div>
+</header>
+<body>
+    <section>
+        
+    </section>
+</body>
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		padding: 0.5em;
+		align-items: center;
+	}
+	header a {
+		text-decoration: none;
+		color: #555;
+		transition: all 0.3s ease 0s;
+	}
+	header a:hover {
+		color: var(--green);
+		text-decoration: underline;
+	}
+	header button {
+		padding: 0.75em 2em;
+	}
+</style>
