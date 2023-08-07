1eac9b6929d5e7b1a3dc5c412ddda3ac302d48eb
Jax-Hax <jaxhack543@gmail.com>
2023-08-07 15:31:57 -0400

    dash

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,32 +1,3 @@
 <script>
-    import Login from './Login.svelte'
-    import Signup from './Signup.svelte'
-    let login = false;
-    export let form;
-    if(form){
-        console.log(form.user);
-    }
+    
 </script>
-{#if login}
-    <Login>
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-        <p class="already" on:click={() => login = false}>Not a user? Sign up</p>
-    </Login>
-{:else}
-    <Signup>
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-        <p class="already" on:click={() => login = true}>Already a user? Log in</p>
-    </Signup>
-{/if}
-<style>
-    .already{
-        cursor: pointer; 
-        text-align: center
-    }
-    .already:hover {
-		color: var(--green);
-		text-decoration: underline;
-	}
-</style>
