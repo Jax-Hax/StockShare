6258114721af18208d9d13970142242ca920eab2
Jax-Hax <jaxhacks543@gmail.com>
2023-08-07 08:44:51 -0700

    trying to add backend idk supabase

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -2,10 +2,10 @@ import {supabase} from '$lib/database'
 export const actions = {
 	signup: async ({ cookies, request }) => {
 		const data = await request.formData();
-        const { signUpData, error } = await supabase.auth.signUp({
-            email: data.get('email'),
-            password: data.get('password'),
-          })
-        console.log(signUpData)
+    const { signUpData, error } = await supabase.auth.signUp({
+        email: data.get('email'),
+        password: data.get('password'),
+      })
+    return {user: signUpData};
 	}
 };
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,13 +2,21 @@
     import Login from './Login.svelte'
     import Signup from './Signup.svelte'
     let login = false;
+    export let form;
+    if(form){
+        console.log(form.user);
+    }
 </script>
 {#if login}
     <Login>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
         <p class="already" on:click={() => login = false}>Not a user? Sign up</p>
     </Login>
 {:else}
     <Signup>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
         <p class="already" on:click={() => login = true}>Already a user? Log in</p>
     </Signup>
 {/if}
