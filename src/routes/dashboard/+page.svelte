376dd160c4d08b28b68c1c296ae6486e906f6387
Jax-Hax <jaxhack543@gmail.com>
2023-08-08 13:43:12 -0400

    get and post are confusing

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,7 +1,25 @@
 <script>
+    //user ID and parties
     export let data;
+    let isLobby = true;
+    let party;
+    async function join(partyID, party){
+        party = party;
+        const response = await fetch('/api', {
+				method: 'POST',
+				body: JSON.stringify({ partyID, data.userID }),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+    }
 </script>
+{#if isLobby}
+{#each data.parties as party}
+    <h1>{party.name}</h1>
+    <p>{party.users}</p>
+    <button on:click={() => join(party.party_id,party)}>Click</button>
+{/each}
+{:else}
 
-{data.userID}
-
-{data.parties}
+{/if}
