946653dcaa4a4771de16eb71068203b7ba6a08af
Jax-Hax <jaxhack543@gmail.com>
2023-08-08 10:34:50 -0400

    error handled

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,12 +1,14 @@
 import {supabase} from '$lib/database';
-
+import {error as svelteError} from '@sveltejs/kit';
 export async function load({ cookies }) {
 	const userID = cookies.get('user');
 	const { data, error } = await supabase
 		.from('usersInParty')
 		.select('*')
 		.eq('user_id',userID)
+		.select('party_id')
 	console.log(error, data);
+	if (error != null) throw svelteError(420, "SQL Error: " + error.message + ", try refreshing the page");
 	return {
 		userID
 	};
