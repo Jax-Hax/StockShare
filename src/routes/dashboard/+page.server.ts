3195df95861d484bee73e954c70124b3e46e6712
Jax-Hax <jaxhack543@gmail.com>
2023-08-08 10:23:07 -0400

    i am an sql god now

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,7 +1,12 @@
 import {supabase} from '$lib/database';
 
-export function load({ cookies }) {
+export async function load({ cookies }) {
 	const userID = cookies.get('user');
+	const { data, error } = await supabase
+		.from('usersInParty')
+		.select('*')
+		.eq('user_id',userID)
+	console.log(error, data);
 	return {
 		userID
 	};
