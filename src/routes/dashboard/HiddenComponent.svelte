ce9daf7f2f85036907f43d31bc6ca8776406f09f
Jax-Hax <jaxhack543@gmail.com>
2023-08-14 13:02:05 -0400

    got hidden to work

---
+++ src/routes/dashboard/HiddenComponent.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    function enable() {
+        dispatch('enable');
+    }
+</script>
+<div id="hidden">
+    <span id="expand" on:click={enable} class="material-symbols-outlined">add</span>
+</div>
+<style>
+    #hidden{
+        background-color: #1e1e1e; 
+        padding: 2em;
+        margin: 2em;
+        border-radius: 3em;
+        cursor: pointer;
+    }
+    #expand{
+        color: white;
+        font-size: 35px;
+        user-select: none;
+    }
+</style>
