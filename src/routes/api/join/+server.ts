c5fb3fef5643fd260d4e53b82570685d60fb0999
Jax-Hax <jaxhack543@gmail.com>
2023-08-14 19:02:10 -0400

    forgot to delete those the vercel build failed

--- src/routes/api/join/+server.ts
+++
@@ -1,9 +0,0 @@
-import { supabase } from '$lib/database';
-import { json } from '@sveltejs/kit';
-import yahooFinance from 'yahoo-finance2';
-
-export async function POST({ request, cookies }) {
-	const { partyID } = await request.json();
-	cookies.set('party_id',partyID,{ path: '/' })
-	return new Response(null, { status: 204 });
-}
